a58fe1c4b58b35a05c66da0d9f38e2c9aba4cfd1
z00231689524 <tdma000@qq.com>
2017-06-04 17:45:23 +0800

    Update url_manager.py
    update

--- url_manager.py
+++ url_manager.py
@@ -4,13 +4,13 @@ class UrlManager(object):
     def __init__(self):
         self.new_urls = set()
         self.old_urls = set()
-
+#添加一个url
     def add_new_url(self, url):
         if url is None:
             return
         if url not in self.new_urls and url not in self.old_urls:
             self.new_urls.add(url)
-
+#添加多个url
     def add_new_urls(self, urls):
         if urls is None or len(urls) == 0:
             return
@@ -25,6 +25,3 @@ class UrlManager(object):
         new_url = self.new_urls.pop()
         self.old_urls.add(new_url)
         return new_url
-
-
-
