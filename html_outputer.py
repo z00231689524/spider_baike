efc1b0a0d63c1a3f04859d78a8461fc76c318d93
z00231689524 <tdma000@qq.com>
2017-06-04 17:43:51 +0800

    Update html_outputer.py
    update

--- html_outputer.py
+++ html_outputer.py
@@ -4,12 +4,16 @@ class HtmlOutputer(object):
 
     def __init__(self):
         self.datas = []
+        self.datalist = [] ###加的 list
 
 
-    def collect_data(self, data):
+    def collect_data(self, data, datalist):
         if data is None:
             return
+        if datalist is None:###加的 list
+            return
         self.datas.append(data)
+        self.datalist.append(datalist)###加的 list
 
     def output_html(self):
         fout = open('output.html', 'w')
@@ -22,6 +26,13 @@ class HtmlOutputer(object):
             fout.write("<td>%s</td>" % data['url'])
             fout.write("<td>%s</td>" % data['title'].encode('utf-8'))
             fout.write("<td>%s</td>" % data['summary'].encode('utf-8'))
+            fout.write("</tr>")
+
+        if self.datalist is None:###加的 list
+            return
+        for data in self.datalist:
+            fout.write("<tr>")
+            fout.write("<td>%s</td>" % data)
             fout.write("</tr>")
         fout.write("</table>")
         fout.write("</body>")
