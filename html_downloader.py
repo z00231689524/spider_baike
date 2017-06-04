ea6a02cfe246a6e91747f5577718bc627818a18e
z00231689524 <tdma000@qq.com>
2017-06-04 17:42:00 +0800

    Update html_downloader.py
    urllib2 --》 requests

--- html_downloader.py
+++ html_downloader.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python2
 # -*- coding: UTF-8 -*-
-import urllib2
+#import urllib2
+#class HtmlDownloader(object):
+
+#    def download(self, url):
+#        if url is None:
+#            return None
+#        response = urllib2.urlopen(url)
+#        if response.getcode() != 200:
+#            return None
+#        return response.read()
 
 
+import requests
 class HtmlDownloader(object):
-
     def download(self, url):
         if url is None:
             return None
-        response = urllib2.urlopen(url)
-        if response.getcode() != 200:
+        response = requests.get(url)
+        if response.status_code != 200:
             return None
-        return response.read()
+        htmlencode = response.encoding
+        return response.text.encode(htmlencode)
